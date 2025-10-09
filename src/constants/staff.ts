91785d969890ffe26adb5000a7b17ae513f80339
tokyocn <tokyocn@hotmail.com>
2025-10-09 11:56:14 +0900

    staff的定义修改

--- src/constants/staff.ts
+++ src/constants/staff.ts
@@ -1,7 +1,5 @@
 export const STAFF_USER_IDS = [
   "lycapd3O2adB0dshwUHWEdQwDHv2",
-  "RaSoRvReD6craTlcg5YW95hcZL33",
-  "toDMjjsguBSTbjCdq4GVon5x9W93",
-  "bSbaTqVEbvfL0onTVlU6LUh3A4H3",
+  "ezEmouH5OsaQg6jdWdnCPNndkMv2",
   "8s72qpqWHFWElWcf5I2tvSaU1jy1",
 ];
